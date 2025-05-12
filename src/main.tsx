96e504658021c6025b4209c3784e0798a769ac14
kennylisal <kennylisal5@gmail.com>
2025-05-12 21:58:14 +0800

    penambahan layout schedule-movie

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import MoviesView from "./movies/index.tsx";
+// import MoviesView from "./public/movies/index.tsx";
+// import MovieScheduleView from "./admin/movie-schedule/index.tsx";
+import PercobaanView from "./admin/movie-schedule/percobaan.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MoviesView />
+    <PercobaanView />
   </StrictMode>
 );
---
+++ src/public/movies/interfaces.ts
@@ -0,0 +1,19 @@
+interface MovieDisplay {
+  name: string;
+  picture: string;
+  genres: string[];
+}
+
+interface MovieObject {
+  created_at: Date;
+  external_info?: {
+    genres: string[];
+    overview: string;
+    release_date: string;
+  };
+  external_link?: string;
+  movie_duration: number;
+  movie_image: string;
+  movie_name: string;
+}
+export type { MovieDisplay, MovieObject };
